117c425f7e705945140e32455992eef241608bc8
mathiasbk1 <mathias.b.k@hotmail.com>
2021-04-14 13:47:23 +0200

    first

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# Letter frequencies
-Simple program used to illustrate performance problems. You should be able to optimize this program to run about twice as fast.
+ASD
--- src/main/java/cphbusiness/ufo/letterfrequencies/Main.java
+++ src/main/java/cphbusiness/ufo/letterfrequencies/Main.java
@@ -4,10 +4,14 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 import static java.util.stream.Collectors.toMap;
 
 /**
@@ -16,15 +20,68 @@ import static java.util.stream.Collectors.toMap;
  *
  * @author kasper
  */
+
 public class Main {
+    static class Timer {
+        private long start, spent = 0;
+
+        public Timer() {
+            play();
+        }
+
+        public double check() {
+            return (System.nanoTime() - start + spent) / 1e9;
+        }
+
+        public void pause() {
+            spent += System.nanoTime() - start;
+        }
+
+        public void play() {
+            start = System.nanoTime();
+        }
+    }
 
     public static void main(String[] args) throws FileNotFoundException, IOException {
-        String fileName = "/Users/kasper/GITHUB/ufo/letterfrequencies/src/main/resources/FoundationSeries.txt";
+
+//        orginalprint();
+//
+//        System.out.println("***************");
+//        orginal2();
+
+        Mark5();
+
+    }
+
+    private static void orginal() throws IOException {
+        String fileName = "D:\\DatSoft2021Spring\\UFO\\letterfrequencies\\src\\main\\resources\\FoundationSeries.txt";
         Reader reader = new FileReader(fileName);
-        Map<Integer, Long> freq = new HashMap<>();
+        HashMap<Integer, Long> freq = new HashMap<>();
+        tallyChars(reader, freq);
+    }
+
+    private static void orginal2() throws IOException {
+        String fileName = "D:\\DatSoft2021Spring\\UFO\\letterfrequencies\\src\\main\\resources\\FoundationSeries.txt";
+
+        Path filepath = Paths.get(fileName);
+
+        Map<Integer, Long> freq =
+                Files.lines(filepath, Charset.forName("UTF-8"))
+                        .flatMapToInt(String::chars)
+                        .boxed()
+                        .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+
+    }
+
+    private static void orginalprint() throws IOException {
+        String fileName = "D:\\DatSoft2021Spring\\UFO\\letterfrequencies\\src\\main\\resources\\FoundationSeries.txt";
+        Reader reader = new FileReader(fileName);
+        HashMap<Integer, Long> freq = new HashMap<>();
         tallyChars(reader, freq);
         print_tally(freq);
     }
+
 
     private static void tallyChars(Reader reader, Map<Integer, Long> freq) throws IOException {
         int b;
@@ -33,9 +90,17 @@ public class Main {
                 freq.put(b, freq.get(b) + 1);
             } catch (NullPointerException np) {
                 freq.put(b, 1L);
-            };
+            }
         }
     }
+
+//    private static void tallyCharsv1(Reader reader, Map<Integer, Long> freq) throws IOException {
+//
+//
+//        while ((b = reader.read()) != -1) {
+//            freq.put(b, freq.getOrDefault(b, 0L) + 1);
+//        }
+//    }
 
     private static void print_tally(Map<Integer, Long> freq) {
         int dist = 'a' - 'A';
@@ -51,7 +116,42 @@ public class Main {
                         toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e2,
                                 LinkedHashMap::new));
         for (Character c : sorted.keySet()) {
-            System.out.println("" + c + ": " + sorted.get(c));;
+            System.out.println("" + c + ": " + sorted.get(c));
+
         }
     }
+
+
+    public static void Mark5() throws IOException {
+        int n = 10, count = 1, totalCount = 0;
+        //HashMap<Integer, Long> dummy = null;
+        double runningTime = 0.0;
+
+        do {
+            count *= 2;
+            double st = 0.0, sst = 0.0;
+            for (int j = 0; j < n; j++) {
+                Timer t = new Timer();
+                for (int i = 0; i < count; i++) {
+
+
+                    orginal2();
+                    //dummy = tallyChars();
+                    //dummy += multiply(i);
+                }
+                runningTime = t.check();
+                double time = runningTime * 1e9 / count;
+                st += time;
+                sst += time * time;
+                totalCount += count;
+            }
+            double mean = st / n, sdev = Math.sqrt((sst - mean * mean * n) / (n - 1));
+            System.out.printf("%6.1f ns +/- %8.2f %10d%n", mean, sdev, count);
+        } while (runningTime < 2 && count < Integer.MAX_VALUE / 2);
+
+
+        //return dummy;
+    }
+
+
 }
